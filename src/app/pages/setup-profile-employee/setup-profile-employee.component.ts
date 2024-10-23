d35446fbdbdc8236a40135b4ba23eaa19666dc45
bopphao29 <bopphao29@gmail.com>
2024-10-23 17:48:08 +0700

    style all input

--- src/app/pages/setup-profile-employee/setup-profile-employee.component.ts
+++ src/app/pages/setup-profile-employee/setup-profile-employee.component.ts
@@ -15,6 +15,7 @@ import { NzUploadChangeParam, NzUploadFile, NzUploadModule } from 'ng-zorro-antd
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, FormArray,Validators  } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { IData } from '../../models-employee/setup-profile-employee/index.model';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-setup-profile-employee',
@@ -35,6 +36,7 @@ import { IData } from '../../models-employee/setup-profile-employee/index.model'
     NzModalModule,
     NzDatePickerModule,
     NzTabsModule,
+    NzUploadModule,
     
 
   ],
@@ -46,7 +48,7 @@ export class SetupProfileEmployeeComponent implements OnInit {
   date = null;
   isEnglish = false;
   fileList: NzUploadFile[] = [];
-
+  fileCompressed: File[] = []
   form: FormGroup;
 
   data: IData= {
@@ -98,7 +100,12 @@ export class SetupProfileEmployeeComponent implements OnInit {
   }
 
   file = true
+  isShowModalUploadfile = false
   isModalInforEmployee = false
+  isDriver : boolean = false
+  loading = false;
+  avatarUrl?: string;
+
 
   constructor(
     private fb: FormBuilder,
@@ -156,9 +163,9 @@ export class SetupProfileEmployeeComponent implements OnInit {
     lastModifiedDate: null,
     newFile: null,
     documents : this.fb.array([this.createArchivedRecords()]),
-    childStatus: this.fb.array([this.createChildStatus()])
+    childStatus: this.fb.array([this.createChildStatus()]),
     })    
-    
+    this.checkDriver()
   }
 
   get documents(): FormArray{
@@ -212,4 +219,82 @@ export class SetupProfileEmployeeComponent implements OnInit {
     this.isModalInforEmployee = false;
   }
 
+  //check driver => show input
+  checkDriver(){
+    this.form.get('departmentId')?.valueChanges.subscribe((value: any)=> {
+      this.isDriver = value === '4';
+
+    })
+  } 
+
+  beforeUpload = (file: NzUploadFile, _fileList: NzUploadFile[]): Observable<boolean> =>
+    new Observable((observer: Observer<boolean>) => {
+      const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+      if (!isJpgOrPng) {
+        this.msg.error('You can only upload JPG file!');
+        observer.complete();
+        return;
+      }
+      const isLt2M = file.size! / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        this.msg.error('Image must smaller than 2MB!');
+        observer.complete();
+        return;
+      }
+      observer.next(isJpgOrPng && isLt2M);
+      observer.complete();
+    });
+
+    private getBase64(img: File, callback: (img: string) => void): void {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => callback(reader.result!.toString()));
+      reader.readAsDataURL(img);
+    }
+
+    handleChangeFile(info: { file: NzUploadFile }): void {
+      switch (info.file.status) {
+        case 'uploading':
+          this.loading = true;
+          break;
+        case 'done':
+          // Get this url from response in real world.
+          this.getBase64(info.file!.originFileObj!, (img: string) => {
+            this.loading = false;
+            this.avatarUrl = img;
+          });
+          break;
+        case 'error':
+          this.msg.error('Network error');
+          this.loading = false;
+          break;
+      }
+    }
+
+    onFileSelected(event : any): Observable<File>{
+      return new Observable((observer: Observer<File>)=> {
+        const file : File = event.target.files[0];
+        if(file){
+          this.fileCompressed = [file]
+          observer.next(file)
+          observer.complete()
+        }
+        else{
+          observer.error('Error')
+        }
+      })
+    }
+
+    onFileChange(event : any): void{
+      this.onFileSelected(event).subscribe({
+        next : (file: File) => {
+
+        },
+        error: (err) =>{
+          console.log(err)
+        },
+        complete: ()=> {
+          
+        }
+      })
+    }
 }
